40335e48094f83498faac64a1559b6aedc6dbfdb
Antholem <antholemlemmanalo@gmail.com>
2024-03-03 10:13:54 +0800

    Navigation Bar and Route Setup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-import React, { useEffect, useState } from "react";
-import SignIn from "./pages/signin/page";
-import Home from "./pages/home/page";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { auth } from "./config/firebase";
-import CustomizedSpinner from "./components/spinner/component";
+import React, { Fragment } from "react";
+import StructuredRoute from "./routes/structured-route";
 
 function App() {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = React.useState(true);
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setUser(user);
-      setLoading(false);
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-  if (loading) {
-    return (
-      <CustomizedSpinner />
-    );
-  }
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/signin" element={user ? <Navigate to="/" /> : <SignIn />} />
-        <Route path="/" element={user ? <Home /> : <Navigate to="/signin" />} />
-      </Routes>
-    </BrowserRouter>
+    <Fragment>
+      <StructuredRoute />
+    </Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
-
-reportWebVitals();
