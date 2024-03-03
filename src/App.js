155ee9f5f3c1c81d5d1ba0bef6dbc8fc473e106e
Antholem <antholemlemmanalo@gmail.com>
2024-03-03 09:15:21 +0800

    Customized Spinner

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import React, { useEffect, useState } from "react";
-import { ChakraProvider } from "@chakra-ui/react";
 import SignIn from "./pages/signin/page";
 import Home from "./pages/home/page";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { auth } from "./config/firebase";
+import CustomizedSpinner from "./components/spinner/component";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = React.useState(true);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setUser(user);
+      setLoading(false);
     });
 
     return () => unsubscribe();
   }, []);
 
+  if (loading) {
+    return (
+      <CustomizedSpinner />
+    );
+  }
+
   return (
-    <ChakraProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signin" element={user ? <Navigate to="/" /> : <SignIn />}/>
-          <Route path="/" element={user ? <Home /> : <Navigate to="/signin" />} />
-        </Routes>
-      </BrowserRouter>
-    </ChakraProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={user ? <Navigate to="/" /> : <SignIn />} />
+        <Route path="/" element={user ? <Home /> : <Navigate to="/signin" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
