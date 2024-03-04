d6a0e87709fa252a3e566d5cf9ec02106095a3fd
Antholem <antholemlemmanalo@gmail.com>
2024-03-04 20:36:12 +0800

    Dynamic Routing, Creating Documents, and Subcollections

--- src/pages/collections/photos.js
+++ src/pages/collections/photos.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getFirestore, doc, getDoc, collection, addDoc, getDocs, onSnapshot } from 'firebase/firestore';
-import { Box, Button, Input } from '@chakra-ui/react';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
 const Photos = () => {
     const { id } = useParams();
     const [album, setAlbum] = useState(null);
-    const [photoTitle, setPhotoTitle] = useState('');
-    const [photos, setPhotos] = useState([]);
 
     useEffect(() => {
         const fetchAlbum = async () => {
@@ -16,13 +13,6 @@ const Photos = () => {
             const albumSnapshot = await getDoc(albumRef);
             if (albumSnapshot.exists()) {
                 setAlbum({ ...albumSnapshot.data(), id: albumSnapshot.id });
-
-                const photosRef = collection(albumRef, albumSnapshot.data().title);
-                const unsubscribe = onSnapshot(photosRef, (snapshot) => {
-                    setPhotos(snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
-                });
-
-                return () => unsubscribe();
             } else {
                 console.log('Album not found');
             }
@@ -31,40 +21,10 @@ const Photos = () => {
         fetchAlbum();
     }, [id]);
 
-    const addPhoto = async () => {
-        const db = getFirestore();
-        const albumRef = doc(db, 'albums', id);
-        const photosRef = collection(albumRef, album.title);
-
-        try {
-            await addDoc(photosRef, { title: photoTitle });
-            setPhotoTitle('');
-            console.log("Photo added successfully!");
-        } catch (error) {
-            console.error("Error adding photo: ", error);
-        }
-    };
-
     return (
         <div>
             {album && <div>{album.title}</div>}
-            <Box>
-                <Input
-                    placeholder={`Input ${album?.title} Title`}
-                    value={photoTitle}
-                    onChange={(e) => setPhotoTitle(e.target.value)}
-                />
-            </Box>
-            <Box>
-                <Button onClick={addPhoto} colorScheme='blue'>
-                    Add {album?.title}
-                </Button>
-            </Box>
-            <Box>
-                {photos.map(photo => (
-                    <div key={photo.id}>{photo.title}</div>
-                ))}
-            </Box>
+            {/* Display photos here */}
         </div>
     );
 };
