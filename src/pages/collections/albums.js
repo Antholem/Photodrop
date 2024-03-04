3ebc63f0d680b952bfacddde2433163cc4e724a8
Antholem <antholemlemmanalo@gmail.com>
2024-03-04 22:08:04 +0800

    Dynamic Routing, Creating Documents, and Subcollections

--- src/pages/collections/albums.js
+++ src/pages/collections/albums.js
@@ -1,27 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { auth } from "../../config/firebase";
-import { Box} from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import { getFirestore, collection, onSnapshot } from "firebase/firestore";
 
 const Collections = () => {
-    const [user, setUser] = useState(null);
     const [albums, setAlbums] = useState([]);
 
     useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-            setUser(user);
-        });
-
-        return () => unsubscribe();
-    }, []);
-
-    useEffect(() => {
         const db = getFirestore();
-        const albumCollections = collection(db, "albums");
-
-        const unsubscribe = onSnapshot(albumCollections, (querySnapshot) => {
-            const albumsData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-            setAlbums(albumsData);
+        const unsubscribe = onSnapshot(collection(db, "albums"), (snapshot) => {
+            const albumList = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                title: doc.data().title,
+            }));
+            setAlbums(albumList);
         });
 
         return () => unsubscribe();
@@ -30,15 +21,13 @@ const Collections = () => {
     return (
         <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" h="100vh" gap={2}>
             <Box>
-                Album List:
+                <Text fontSize="2xl">Album List:</Text>
+                {albums.map((album) => (
+                    <Text key={album.id}>{album.title}</Text>
+                ))}
             </Box>
-            {albums.map((album) => (
-                <Box key={album.id}>
-                    {album.title}
-                </Box>
-            ))}
         </Box>
-    )
-}
+    );
+};
 
 export default Collections;
