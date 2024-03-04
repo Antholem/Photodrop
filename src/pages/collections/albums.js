e0733ea5c32dc155424f57c44f0802f1e04967e7
Antholem <antholemlemmanalo@gmail.com>
2024-03-04 22:01:09 +0800

    Dynamic Routing, Creating Documents, and Subcollections

--- src/pages/collections/albums.js
+++ src/pages/collections/albums.js
@@ -1,44 +1,44 @@
-// import React, { useEffect, useState } from "react";
-// import { auth } from "../../config/firebase";
-// import { Box} from "@chakra-ui/react";
-// import { getFirestore, collection, onSnapshot } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { auth } from "../../config/firebase";
+import { Box} from "@chakra-ui/react";
+import { getFirestore, collection, onSnapshot } from "firebase/firestore";
 
-// const Collections = () => {
-//     const [user, setUser] = useState(null);
-//     const [albums, setAlbums] = useState([]);
+const Collections = () => {
+    const [user, setUser] = useState(null);
+    const [albums, setAlbums] = useState([]);
 
-//     useEffect(() => {
-//         const unsubscribe = auth.onAuthStateChanged((user) => {
-//             setUser(user);
-//         });
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            setUser(user);
+        });
 
-//         return () => unsubscribe();
-//     }, []);
+        return () => unsubscribe();
+    }, []);
 
-//     useEffect(() => {
-//         const db = getFirestore();
-//         const albumCollections = collection(db, "albums");
+    useEffect(() => {
+        const db = getFirestore();
+        const albumCollections = collection(db, "albums");
 
-//         const unsubscribe = onSnapshot(albumCollections, (querySnapshot) => {
-//             const albumsData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-//             setAlbums(albumsData);
-//         });
+        const unsubscribe = onSnapshot(albumCollections, (querySnapshot) => {
+            const albumsData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+            setAlbums(albumsData);
+        });
 
-//         return () => unsubscribe();
-//     }, []);
+        return () => unsubscribe();
+    }, []);
 
-//     return (
-//         <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" h="100vh" gap={2}>
-//             <Box>
-//                 Album List:
-//             </Box>
-//             {albums.map((album) => (
-//                 <Box key={album.id}>
-//                     {album.title}
-//                 </Box>
-//             ))}
-//         </Box>
-//     )
-// }
+    return (
+        <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" h="100vh" gap={2}>
+            <Box>
+                Album List:
+            </Box>
+            {albums.map((album) => (
+                <Box key={album.id}>
+                    {album.title}
+                </Box>
+            ))}
+        </Box>
+    )
+}
 
-// export default Collections;
+export default Collections;
