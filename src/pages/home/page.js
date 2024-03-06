d7419b21af3edb74040fa0646fde018e0a20aa23
Antholem <antholemlemmanalo@gmail.com>
2024-03-06 22:25:45 +0800

    Firebase Setup

--- src/pages/home/page.js
+++ src/pages/home/page.js
@@ -1,18 +1,7 @@
-import { Fragment, useState, useEffect } from "react";
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../../config/firebase';
+import { Fragment } from "react";
 import { Box, Text } from "@chakra-ui/react";
 
 function Home() {
-    const [user, setUser] = useState();
-
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            setUser(user);
-        });
-
-        return () => unsubscribe();
-    }, []);
 
     return (
         <Fragment>
