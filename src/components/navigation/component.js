0d6c541ffe7fdb0ba8fe07c08135d3112cb29ac0
Antholem <antholemlemmanalo@gmail.com>
2024-03-06 22:15:53 +0800

    Navigation

--- src/components/navigation/component.js
+++ src/components/navigation/component.js
@@ -1,13 +1,35 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
-import { Box } from "@chakra-ui/react";
+import { auth } from '../../config/firebase';
+import { onAuthStateChanged , signOut} from 'firebase/auth';
+import { Box, Button } from "@chakra-ui/react";
 
 const Navigation = () => {
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    const signOutHandler = async () => {
+        try {
+            await signOut(auth);
+            window.alert('Sign Out Success');
+        } catch (error) {
+            window.alert(error);
+        }
+    };
 
     return (
         <Fragment>
             <Box bg="white" w="100%" p={4} boxShadow='md' position='sticky' top={0}>
-                Navigation
+                <Button onClick={signOutHandler} colorScheme='blue' variant='solid'>
+                    Sign Out
+                </Button>
             </Box>
             <Outlet />
         </Fragment>
