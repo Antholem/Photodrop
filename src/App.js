349a5c872f34ae9f872a1dc11bde1be7a3938bae
Antholem <antholemlemmanalo@gmail.com>
2024-03-07 17:33:27 +0800

    Subcollections

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './config/firebase';
-import Home from "./pages/home/page";
 import SignIn from "./pages/sign-in/page";
 import CustomizedSpinner from "./components/spinner/component";
 import Navigation from "./components/navigation/component";
 import Fallback from "./pages/fallback/page";
+import Albums from "./pages/databases/albums/pages";
+import Photos from "./pages/databases/photos/page";
 
 function App() {
   const [user, setUser] = useState();
@@ -29,7 +30,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<Navigation />}>
-          <Route path="/" element={user ? <Home /> : <Navigate to="/signin" replace />} />
+          <Route path="/" element={user ? <Albums /> : <Navigate to="/signin" replace />} />
+          <Route path="albums/:id" element={user ? <Photos /> : <Navigate to="/signin" replace />} />
           {user && <Route path="*" element={<Fallback />} />}
         </Route>
         <Route path="/signin" element={!user ? <SignIn /> : <Navigate to="/" replace />} />
